55db2af57489fd7be243f5965ac3966db9e51b93
Jon Lake <francis.j.lake@gmail.com>
2018-04-03 00:51:35 -0400

    Update controller and route for getting user by id

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,28 @@
 const User = require('../models/user');
 
-
+// Handle user creation.
 exports.user_create = async (req, res) => {
     const newUser = req.body;
     const user = await User.query().insert(newUser);
     res.send(user);
 };
 
+// Display list of all users.
 exports.user_list = async (req, res) => {
     const users = await User.query();
     res.send(users);
+};
+
+// Display single user by id.
+exports.user_get_by_id = async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await User.query().findById(userId);
+        return res.send(user);
+    
+    } catch(err) {
+        console.log('Cannot find user with that id.');
+        return res.sendStatus(400);
+    }
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -10,4 +10,7 @@ router.post('/create', user_controller.user_create);
 // GET request for a list of all users.
 router.get('/', user_controller.user_list);
 
+// GET request for user by id.
+router.get('/:id', user_controller.user_get_by_id);
+
 module.exports = router;
